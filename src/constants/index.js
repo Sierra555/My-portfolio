323a647758ab2e40355641c180342613bfb6682b
Sierra555 <dara6234@gmail.com>
2025-05-26 09:11:22 +0300

    upd imports

--- src/constants/index.js
+++ src/constants/index.js
@@ -22,6 +22,7 @@ import {
 	blog,
 	ticket,
 	lpgWebp,
+	chatAi,
 	learnVueWebp,
 	findjobWebp,
 	techstoreWebp,
@@ -34,6 +35,7 @@ import {
 	portfolioWebp,
 	blogWebp,
 	ticketWebp,
+	chatAiWebp,
 } from "../assets";
 
 export const navLinks = [
